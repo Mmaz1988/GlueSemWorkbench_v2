8263f080f1c04d5e4db20878959184ce54e8e23b
Mmaz1988 <mark-matthias.zymla@uni-konstanz.de>
2017-10-31 11:23:44 -0700

    MMAZ: factory reset

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,6 @@ public class Main {
 
     public static void main(String[] args){
 
-        for (int i = 0; i < 10; i++)
-        {
-            if (i%2 == 0) {
-                System.out.println("Hi Moritz!");
-            }
-            else
-            {
-                System.out.print("Bye Moritz!");
-            }
-        }
-        for (int i = 0; i < 50; i++)
-        {
-            System.out.println("GEEK ON!");
-        }
 
-    }
 
 }
