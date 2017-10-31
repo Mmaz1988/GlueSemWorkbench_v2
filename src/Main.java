279e3cd6408e56a8456b18d96da6d5fec57a6ed2
Mmaz1988 <mark-matthias.zymla@uni-konstanz.de>
2017-10-31 11:18:58 -0700

    MMAZ: github test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
 
     public static void main(String[] args){
 
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println("Hi Moritz!");
+        }
     }
 
 }
