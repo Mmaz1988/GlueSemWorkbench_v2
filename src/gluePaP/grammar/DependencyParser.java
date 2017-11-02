7aca047afb2beb04b3d320a9362dc7c0fe1d6c64
Mmaz1988 <mark-matthias.zymla@uni-konstanz.de>
2017-11-02 13:45:37 -0700

    Integrated dependency parser

---
+++ src/gluePaP/grammar/DependencyParser.java
@@ -0,0 +1,48 @@
+package gluePaP.grammar;
+
+import java.io.StringReader;
+import java.util.List;
+
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.process.TokenizerFactory;
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
+import edu.stanford.nlp.process.CoreLabelTokenFactory;
+import edu.stanford.nlp.process.PTBTokenizer;
+import edu.stanford.nlp.process.Tokenizer;
+import edu.stanford.nlp.trees.Tree;
+
+public class DependencyParser {
+    private final static String PCG_MODEL = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz";
+
+    private final TokenizerFactory<CoreLabel> tokenizerFactory = PTBTokenizer.factory(new CoreLabelTokenFactory(), "invertible=true");
+
+    private final LexicalizedParser parser = LexicalizedParser.loadModel(PCG_MODEL);
+
+    public Tree parse(String str) {
+        List<CoreLabel> tokens = tokenize(str);
+        Tree tree = parser.apply(tokens);
+        return tree;
+    }
+
+    private List<CoreLabel> tokenize(String str) {
+        Tokenizer<CoreLabel> tokenizer =
+                tokenizerFactory.getTokenizer(
+                        new StringReader(str));
+        return tokenizer.tokenize();
+    }
+
+    public static void main(String[] args) {
+        String str = "John snores.";
+        DependencyParser parser = new DependencyParser();
+        Tree tree = parser.parse(str);
+
+        List<Tree> leaves = tree.getLeaves();
+        // Print words and Pos Tags
+        for (Tree leaf : leaves) {
+            Tree parent = leaf.parent(tree);
+            System.out.print(leaf.label().value() + "-" + parent.label().value() + " ");
+        }
+        System.out.println();
+    }
+}
+
