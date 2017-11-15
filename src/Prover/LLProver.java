ae1c72c4ef2c6587c6172470e9accc34256b35d4
mmessmer <moritz.messmer@uni-konstanz.de>
2017-11-14 17:05:31 -0800

    Compile() now handles more complex modifier types.

--- src/Prover/LLProver.java
+++ src/Prover/LLProver.java
@@ -91,6 +91,7 @@ public class LLProver {
 
                     Premise new_premise = this.combinePremises(db_premise,curr_premise);
                     if (new_premise != null) {
+                        new_premise.setHistory(db_premise,curr_premise);
                         System.out.println("Combining premises " + db_premise +" and " + curr_premise + " : " + new_premise);
                         if (new_premise.getPremiseIDs().equals(goalIDs)) {
                             solutions.add(new_premise);
@@ -108,6 +109,7 @@ public class LLProver {
                 if (curr_premise.getTerm() instanceof LLFormula) {
                     Premise new_premise = this.combinePremises(curr_premise,db_premise);
                     if (new_premise != null) {
+                        new_premise.setHistory(curr_premise,db_premise);
                         System.out.println("Combining premises " + curr_premise +" and " + db_premise + " : " + new_premise);
 
                         if (new_premise.getPremiseIDs().equals(goalIDs)) {
--- src/gluePaP/linearLogic/Premise.java
+++ src/gluePaP/linearLogic/Premise.java
@@ -1,11 +1,15 @@
 package gluePaP.linearLogic;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Premise {
     private Set<Integer> premiseIDs;
     private LLTerm term;
+    private Premise func;
+    private Premise arg;
 
     public Set<Integer> getPremiseIDs() {
         return premiseIDs;
@@ -32,5 +36,15 @@ public class Premise {
 
     public void setTerm(LLTerm term) {
         this.term = term;
+    }
+
+
+    /*
+    This method keeps track of all the previous derivation steps of a premise.
+    Not required, only for testing purposes
+    */
+    public void setHistory(Premise func, Premise arg) {
+        this.func = func;
+        this.arg = arg;
     }
 }
