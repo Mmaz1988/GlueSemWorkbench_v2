e548df27979b5f08c405ff4268db95da1165e1f5
mmessmer <moritzmessmer@gmail.com>
2019-01-20 14:33:25 +0100

    Fixed copyright header
    Copyright was not updated for this file in last commit.

--- src/main/WorkbenchMain.java
+++ src/main/WorkbenchMain.java
@@ -1,10 +1,18 @@
 package main;/*
- * Copyright 2018 Moritz Messmer and Mark-Matthias Zymla.
- * This file is part of the Glue Semantics Workbench
- * The Glue Semantics Workbench is free software and distributed under the conditions of the GNU General Public License,
- * without any warranty.
- * You should have received a copy of the GNU General Public License along with the source code.
- * If not, please visit http://www.gnu.org/licenses/ for more information.
+ * Copyright 2019 Mark-Matthias Zymla & Moritz Messmer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
 import glueSemantics.lexicon.LexicalEntry;
