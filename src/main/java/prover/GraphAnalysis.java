9146870a9f07f1a45d636eeab6d7a753f01474fc
MMAZ <mark-matthias.zymla@uni-konstanz.de>
2022-11-15 11:55:45 +0100

    added functionality to also inspect premises inside SCCs

--- src/main/java/prover/GraphAnalysis.java
+++ src/main/java/prover/GraphAnalysis.java
@@ -68,7 +68,7 @@ public class GraphAnalysis {
                 } else {
                     jGraphXAdapter.getModel().setStyle(jGraphXAdapter.getVertexToCellMap().get(g), "defaultVertex;fillColor=lightgreen;opacity=50");
                     mxCell mx = ((mxCell) jGraphXAdapter.getVertexToCellMap().get(g));
-                    //TODO open strongly connected graph in new frame on click
+
                     JGraphXAdapter<CGNode,DefaultEdge> jGraphXAdapter1 = new JGraphXAdapter<>((Graph<CGNode,DefaultEdge>) g);
                     jGraphXAdapter1.getStylesheet().getDefaultEdgeStyle().put(mxConstants.STYLE_NOLABEL, "1");
                     jGraphXAdapter1.getModel().beginUpdate();
@@ -103,7 +103,10 @@ public class GraphAnalysis {
                     jGraphXAdapter.getModel().endUpdate();
                     graphComponent = new mxGraphComponent(jGraphXAdapter);
 
-                    graphComponent.getGraphControl().addMouseListener(new MouseAdapter() {
+                    graphComponent.getGraphControl().addMouseListener(new inspectProofAdapter(graphComponent,sccMap));
+                 /*
+
+
                         @Override
                         public void mouseClicked(MouseEvent e) {
                             Object cell = graphComponent.getCellAt(e.getX(), e.getY());
@@ -114,6 +117,7 @@ public class GraphAnalysis {
                                     testFrame.setSize(1200, 800);
                                     testFrame.setModalityType(Dialog.DEFAULT_MODALITY_TYPE);
                                     testFrame.getContentPane().add(sccMap.get(cell));
+                                    sccMap.get(cell).getGraphControl().addMouseListener(new inspectProofAdapter(sccMap.get(cell)));
                                     testFrame.pack();
                                     testFrame.setVisible(true);
                                     testFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
@@ -161,7 +165,15 @@ public class GraphAnalysis {
                                 }
                             }
                         }
-                    });
+                    }
+
+                    */
+
+
+
+
+
+
 
                 //    graphComponent.getGraphControl().addMouseListener();
 
@@ -177,3 +189,126 @@ public class GraphAnalysis {
 
 
             }
+
+class inspectProofAdapter extends MouseAdapter {
+    public mxGraphComponent gc;
+    private HashMap<mxCell,mxGraphComponent> sccMap;
+
+    public inspectProofAdapter(mxGraphComponent gc, HashMap<mxCell,mxGraphComponent> sccMap)
+    {this.gc = gc;
+    this.sccMap = sccMap;}
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        Object cell = gc.getCellAt(e.getX(), e.getY());
+        if (cell instanceof mxCell) {
+            if (this.sccMap != null && sccMap.containsKey(cell)) {
+
+                JDialog testFrame = new JDialog();
+                testFrame.setSize(1200, 800);
+                testFrame.setModalityType(Dialog.DEFAULT_MODALITY_TYPE);
+                sccMap.get(cell).getGraphControl().addMouseListener(new inspectSCCAdapter(sccMap.get(cell)));
+                testFrame.getContentPane().add(sccMap.get(cell));
+                testFrame.pack();
+                testFrame.setVisible(true);
+                testFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+// your function
+            } else {
+                JDialog testFrame = new JDialog();
+                testFrame.setSize(1200, 800);
+                testFrame.setModalityType(Dialog.DEFAULT_MODALITY_TYPE);
+
+                StringBuilder intermediateResultBuilder = new StringBuilder();
+                List<Premise> solutions = new ArrayList();
+
+                for (History h : ((CGNode) ((Graph) ((mxCell) cell).getValue()).vertexSet().stream().findAny().get()).histories)
+                {
+
+                    if (h.parents.size() == 1 && h.parents.stream().findAny().get().isEmpty())
+                    {
+                        solutions.add(h.p);
+                    }
+                    else {
+                        try {
+                            solutions.addAll(h.calculateSolutions());
+                        } catch (VariableBindingException ex) {
+                            throw new RuntimeException(ex);
+                        } catch (ProverException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+                }
+
+                for (Premise h : solutions) {
+                    intermediateResultBuilder.append(h.toString() + " " + h.getPremiseIDs().toString());
+                    intermediateResultBuilder.append("<br>");
+
+                }
+
+                JLabel solutionLabel = new JLabel();
+                solutionLabel.setText("<html><body>" + intermediateResultBuilder.toString() + "</body></html>" );
+
+                testFrame.getContentPane().add(solutionLabel);
+                testFrame.pack();
+                testFrame.setVisible(true);
+                testFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            }
+        }
+    }
+}
+
+class inspectSCCAdapter extends MouseAdapter {
+    public mxGraphComponent gc;
+    private HashMap<mxCell,mxGraphComponent> sccMap;
+
+    public inspectSCCAdapter(mxGraphComponent gc)
+    {this.gc = gc;}
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        Object cell = gc.getCellAt(e.getX(), e.getY());
+        if (cell instanceof mxCell) {
+
+                JDialog testFrame = new JDialog();
+                testFrame.setSize(1200, 800);
+                testFrame.setModalityType(Dialog.DEFAULT_MODALITY_TYPE);
+
+                StringBuilder intermediateResultBuilder = new StringBuilder();
+                List<Premise> solutions = new ArrayList();
+
+                for (History h : ((CGNode) ((mxCell) cell).getValue()).histories)
+                {
+
+                    if (h.parents.size() == 1 && h.parents.stream().findAny().get().isEmpty())
+                    {
+                        solutions.add(h.p);
+                    }
+                    else {
+                        try {
+                            solutions.addAll(h.calculateSolutions());
+                        } catch (VariableBindingException ex) {
+                            throw new RuntimeException(ex);
+                        } catch (ProverException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+                }
+
+                for (Premise h : solutions) {
+                    intermediateResultBuilder.append(h.toString() + " " + h.getPremiseIDs().toString());
+                    intermediateResultBuilder.append("<br>");
+
+                }
+
+                JLabel solutionLabel = new JLabel();
+                solutionLabel.setText("<html><body>" + intermediateResultBuilder.toString() + "</body></html>" );
+
+                testFrame.getContentPane().add(solutionLabel);
+                testFrame.pack();
+                testFrame.setVisible(true);
+                testFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            }
+        }
+    }
